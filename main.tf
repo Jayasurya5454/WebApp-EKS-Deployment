6e7e46b67d348ecd9587b0d6553ae2c14c13384d
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-09-19 21:56:00 +0530

    update

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
