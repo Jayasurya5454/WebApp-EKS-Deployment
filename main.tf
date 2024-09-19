561ff2659d66d57b7a2d8d832434e50a97f895ea
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-09-19 20:48:23 +0530

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 terraform {
   backend "s3" {
     bucket = "tf-rp-states-jais"
@@ -16,7 +17,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", " us-east-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]  
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
