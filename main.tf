52472e9d59f4eae6d7c6fcacd830322d43f97aa0
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-09-19 20:52:00 +0530

    update

--- main.tf
+++ main.tf
@@ -17,12 +17,17 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]  
+  azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "eks" {
@@ -44,7 +49,7 @@ module "eks" {
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
